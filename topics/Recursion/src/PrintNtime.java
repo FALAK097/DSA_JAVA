409a5e589e1bcf8fdb8d61f67f8f6d385e0efcfe
falak <falakgala09@gmail.com>
2023-11-20 12:26:35 +0530

    recursion print N times

---
+++ topics/Recursion/src/PrintNtime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintNtime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printNtime(n);
+        sc.close();
+    }
+
+    public static void printNtime(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println("Hello World");
+        printNtime(n - 1);
+    }
+}

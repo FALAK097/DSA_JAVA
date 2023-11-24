97fc603545a27fc9afd1b705edb6be5f6ee3d4a2
falak <falakgala09@gmail.com>
2023-11-24 15:38:36 +0530

    insertion sort

--- topics/SortingAlgo/InsertionSort/InsertionSort.java
+++ topics/SortingAlgo/InsertionSort/InsertionSort.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BubbleSort {
+public class InsertionSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array: ");
@@ -10,7 +10,7 @@ public class BubbleSort {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        bubblesort(n, arr);
+        insertionsort(n, arr);
         System.out.println("The sorted array is: ");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
@@ -18,17 +18,12 @@ public class BubbleSort {
         sc.close();
     }
 
-    public static void bubblesort(int n, int[] arr) {
-        for (int i = n - 1; i >= 0; i--) {
-            int didSwap = 0;
-            for (int j = 0; j <= i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    arr[j + 1] = arr[j] + arr[j + 1] - (arr[j] = arr[j + 1]);
-                    didSwap = 1;
-                }
-            }
-            if (didSwap == 0) {
-                break;
+    public static void insertionsort(int n, int[] arr) {
+        for (int i = 1; i <= n - 1; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                arr[j - 1] = arr[j] + arr[j - 1] - (arr[j] = arr[j - 1]);
+                j--;
             }
         }
     }
---
+++ topics/SortingAlgo/InsertionSort/InsertionSort.md
@@ -0,0 +1,20 @@
+# Insertion Sort
+
+It is a simple comparison-based sorting algorithm which takes an element and places it in its correct position in the sorted subarray. It is also known as sinking sort.
+
+The algorithm works similar to the way
+we sort playing cards in our hands.
+
+## Algorithm
+
+1. Start from the second element in the array.
+2. Compare the current element with the previous element.
+3. If the current element is smaller than the previous element, then swap the elements.
+4. Move the previous element one position forward.
+5. Repeat the steps 2-4 until the end of the array.
+6. Repeat the steps 1-5 for each element in the array.
+
+## Complexity Analysis
+
+- Time Complexity: O(n^2)
+- Space Complexity: O(1)
